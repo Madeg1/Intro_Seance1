1ec762b35003b31ab791a50c2e9242e0ed7713ca
Madeg-c <madeg.collet8@etu.univ-lorraine.fr>
2025-06-16 11:46:48 +0200

    Seance1

---
+++ app/src/main/java/fr/minesnancy/collet/helloworld/MainActivity.java
@@ -0,0 +1,40 @@
+package fr.minesnancy.collet.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    Toast toast;
+    public void perdu(View view) {
+        toast = new Toast(this);
+
+        toast.setText("Mauvaise réponse...");
+        toast.show();
+    }
+
+    public void bravo(View view) {
+        toast = new Toast(this);
+
+        toast.setText("Bravo !");
+        toast.show();
+    }
+}
